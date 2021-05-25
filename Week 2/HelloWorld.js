94d2dd1b9ef78c649ede3b7af243568f92561fd6
Evan <evanjensen95@gmail.com>
2021-05-25 15:22:46 -0700

    final

---
+++ Week 2/HelloWorld.js
@@ -0,0 +1,32 @@
+// const readline = require('readline');
+
+// // creating variables
+// const myFirstName = 'evan';
+// let myAge = 25;
+// console.log('My name is' + myFirstName + 'and I am' + myAge)
+
+// console.log(myFirstName, myAge)  
+// const isCool = true
+// let myHeight = null; // null
+// let myWeight; // undefined
+// let num1 = 7;
+// let num2 = 10;
+// let num3 = 6 + 45;
+// let num4 = num1 + num2;
+
+// // Getting User Input
+// const reader = readline.createInterface({
+//     input: process.stidin,
+//     output: process.stdout
+// })
+// eader.question("Enter a number to find out if it is odd or even: ", function(number) {
+//     if (number % 2 === 0) {
+//         console.log('Even!')
+//         else {
+//             console.log('Odd!')
+//         }
+//     }
+
+// }
+// });
+console.log('random')
